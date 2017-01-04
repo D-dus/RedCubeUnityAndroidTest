23901f01b01def18357cdcc99f7269c43e270dc7
D-dus <khadimoullah.vencatasamy@gmail.com>
2017-01-04 11:06:20 +0100

    First Commit

---
+++ Assets/Script/SpinTheCube.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinTheCube : MonoBehaviour {
+
+	public float mSpeed;
+	// Use this for initialization
+	void Start()
+	{
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+//		if (Input.GetKeyDown (KeyCode.Escape))
+//			Application.Quit ();
+//		transform.Rotate (Vector3.up, mSpeed * Time.deltaTime);
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) 
+		{
+			// Get movement of the finger since last frame
+			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+			// Move object across XY plane
+			transform.Translate(-touchDeltaPosition.x * mSpeed, -touchDeltaPosition.y * mSpeed, 0);
+		}
+	}
+}
